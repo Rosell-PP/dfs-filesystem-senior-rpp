fb8227a6b201c993268e5586fcf60bf271b3fcd1
Rosell Pupo Polanco <rpupopolanco@gmail.com>
2024-10-30 14:15:08 -0400

    Adicionando el servicio para ejecutar el frontend desde un container

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+      <v-img
+        class="mb-4"
+        height="150"
+        src="@/assets/logo.png"
+      />
+
+      <div class="text-center">
+        
+      </div>
+      
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
